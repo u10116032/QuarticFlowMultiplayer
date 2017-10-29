7a78bb5800679e2b598374e5053b7eede486dc9f
Halley <u10116032@gmail.com>
2017-10-29 16:54:46 +0800

    Fix ThirdClient UDP Receiver

--- ThirdClient/Assets/Script/TestThirdPart.cs
+++ ThirdClient/Assets/Script/TestThirdPart.cs
@@ -26,7 +26,7 @@ public class TestThirdPart : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void OnDestroy () 
+	void OnApplicationQuit () 
 	{
 		thirdPartManager.StopReceive ();
 	}
